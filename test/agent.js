73c7136ea54ff9b3a50c3804fe05bc9409126884
Jake Luer <jake.luer@gmail.com>
2013-01-16 08:18:38 -0500

    test: [live-agent] change tests to only run if key/cert is available

--- test/agent.js
+++ test/agent.js
@@ -1,12 +1,22 @@
-var join = require('path').join
+var exists = require('fs').existsSync
+  , join = require('path').join
   , read = require('fs').readFileSync;
 
+var cert = join(__dirname, 'certs/apnagent-cert.pem')
+  , key = join(__dirname, 'certs/apnagent-key-noenc.pem');
+
 describe('Agent', function () {
+
+  if (!exists(cert) || !exists(key)) {
+    it('skipping live agent tests. cert/key files missing.');
+    return;
+  }
+
   it('should be able to connect', function (done) {
     var agent = new apnagent.Agent();
     agent.enable('sandbox');
-    agent.set('cert file', join(__dirname, '/certs/apnagent-cert.pem'));
-    agent.set('key file', join(__dirname, '/certs/apnagent-key-noenc.pem'));
+    agent.set('cert file', cert);
+    agent.set('key file', key);
     agent.connect(function (err) {
       should.not.exist(err);
       agent.once([ 'gateway', 'close' ], done);
@@ -17,8 +27,8 @@ describe('Agent', function () {
   it('should be able to reconnect', function (done) {
     var agent = new apnagent.Agent();
     agent.enable('sandbox');
-    agent.set('cert', read(__dirname + '/certs/apnagent-cert.pem'));
-    agent.set('key', read(__dirname + '/certs/apnagent-key-noenc.pem'));
+    agent.set('cert', read(cert));
+    agent.set('key', read(key));
     agent.connect(function (err) {
       var reconnected = false;
 
