da2d12620bb10c9c671dc8521b9a5df1f5a4a992
Jake Luer <jake.luer@gmail.com>
2013-02-13 16:49:40 -0500

    examples: [basic] load key from certs folder

--- examples/basic.js
+++ examples/basic.js
@@ -1,14 +1,14 @@
 var apnagent = require('..')
   , fs = require('fs')
   , join = require('path').join;
-
-var cert, key;
+var cert, key, device;
 
 try {
   cert = fs.readFileSync(join(__dirname, '../test/certs/apnagent-cert.pem'));
   key = fs.readFileSync(join(__dirname, '../test/certs/apnagent-key-noenc.pem'));
+  device = fs.readFileSync(join(__dirname, '../test/certs/device.txt'));
 } catch (ex) {
-  console.error('Error loading key/cert: %s', ex.message);
+  console.error('Error loading key/cert/device: %s', ex.message);
   process.exit(1);
 }
 
@@ -20,13 +20,14 @@ agent
   .enable('sandbox');
 
 agent.createMessage()
-  .device('5b51030dd5bad758fbad5004bad35c31e4e0f550f77f20d4f737bf8d3d5524c6')
-  .alert('body', 'Hello World')
+  .device('515b030dd5bad758fbad5004bad35c31e4e0f550f77f20d4f737bf8d3d5524c6')
+  .alert('body', 'Hello Universe')
   .send();
 
 agent.createMessage()
-  .device('515b030dd5bad758fbad5004bad35c31e4e0f550f77f20d4f737bf8d3d5524c6')
-  .alert('body', 'Hello Universe')
+  .device(device)
+  .alert('body', 'Hello Universe (first)')
+  .expires('10m')
   .send();
 
 agent.connect();
